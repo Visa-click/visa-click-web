8e9457339f110413ae226162ca6b1dbdb7c0e21d
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-08-06 13:13:38 +0300

    add agreement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,37 @@
+<script lang="ts">
+	import Router, { push } from "svelte-spa-router";
+
+	import Agreement from "../components/Agreement.svelte";
+	import Home from "../components/Home.svelte";
+
+	const routes = {
+		"/": Home,
+		"/agreement": Agreement,
+	};
+
+	let location = "";
+
+	const handleRoute = async (event: any) => {
+		location = event.detail.route;
+	};
+
+	let authChecked = false;
+	let authOk = false;
+
+	$: if (
+		authChecked &&
+		!authOk &&
+		location !== "/agreement" &&
+		location !== "/"
+	) {
+		push("/agreement");
+	} else if (authChecked && authOk && location === "/agreement") {
+		push("/");
+	}
+</script>
+
 <svelte:head>
 	<title>Visa Click</title>
 </svelte:head>
+
+<Router {routes} on:routeLoaded={handleRoute} />
